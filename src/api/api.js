1d255023ab235d22d27457d2618440d84c2ca4be
Jimmy Mackay <j.mackay@backinmotion.com.au>
2018-12-17 14:36:28 +1100

    test

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,7 @@ import axios from 'axios'
 
 export default() => {
     return axios.create({
-        baseURL: `http://localhost:8080/api`,
-        // jmdb.cwcxibundeox.ap-southeast-2.rds.amazonaws.com
-        // root
-        // JKaws123!
-        // db: practice
-        
+        baseURL: `http://localhost:3000/api`,
         withCredentials: false,
         headers: {
             'Accept': 'application/json',
